cbfead5c0e6b46b8460ff52f3c3432d34e570b5b
Neil Wheeler <neil.wheeler@juriba.com>
2022-02-21 17:54:11 +0000

    Add device feed functions (#12)
    New device feed functions.

---
+++ Juriba.Dashworks/Public/New-DwImportDeviceFeed.ps1
@@ -0,0 +1,69 @@
+#requires -Version 7
+function New-DwImportDeviceFeed {
+    <#
+        .SYNOPSIS
+        Creates a new device feed.
+
+        .DESCRIPTION
+        Creates a new deivce feed using the import API.
+        Takes the feed name and an enabled boolean.
+
+        .PARAMETER Instance
+
+        Dashworks instance. For example, myinstance.dashworks.app
+
+        .PARAMETER Port
+
+        Dashworks API port number. Default = 8443
+
+        .PARAMETER APIKey
+
+        Dashworks API Key.
+
+        .PARAMETER Name
+
+        The name of the new device feed.
+
+        .PARAMETER Enabled
+
+        Should the new feed be enabled. Default = True.
+
+        .EXAMPLE
+
+        PS> New-DwImportDeviceFeed -Name "My New Import" -Instance "myinstance.dashworks.app" -APIKey "xxxxx"
+
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Instance,
+        [Parameter(Mandatory=$false)]
+        [int]$Port = 8443,
+        [Parameter(Mandatory=$true)]
+        [string]$APIKey,
+        [parameter(Mandatory=$true)]
+        [string]$Name,
+        [parameter(Mandatory=$false)]
+        [bool]$Enabled = $true
+    )
+
+    $uri = "https://{0}:{1}/apiv2/imports/devices" -f $Instance, $Port
+    $headers = @{'x-api-key' = $APIKey}
+
+    $payload = @{}
+    $payload.Add("name", $Name)
+    $payload.Add("enabled", $Enabled)
+
+    $JsonBody = $payload | ConvertTo-Json
+
+    try {
+        if ($PSCmdlet.ShouldProcess($Name)) {
+            $result = Invoke-RestMethod -Uri $uri -Method POST -Headers $headers -ContentType "application/json" -Body $jsonBody
+            return $result
+        }
+    }
+    catch {
+        Write-Error ("{0}. {1}" -f $_.Exception.Response.StatusCode.Value__, ($_ | ConvertFrom-Json).details)
+        break
+    }
+}
---
+++ Juriba.Dashworks/Public/Set-DwImportDeviceFeed.ps1
@@ -0,0 +1,86 @@
+#requires -Version 7
+function Set-DwImportDeviceFeed {
+    <#
+        .SYNOPSIS
+        Updates a device feed.
+
+        .DESCRIPTION
+        Updates a deivce feed using the import API.
+        Takes the new name and/or enabled status.
+
+        .PARAMETER Instance
+
+        Dashworks instance. For example, myinstance.dashworks.app
+
+        .PARAMETER Port
+
+        Dashworks API port number. Default = 8443
+
+        .PARAMETER APIKey
+
+        Dashworks API Key.
+
+        .PARAMETER ImportId
+
+        Id of feed to be updated.
+
+        .PARAMETER Name
+
+        The name of the new device feed.
+
+        .PARAMETER Enabled
+
+        Should the new feed be enabled.
+
+        .EXAMPLE
+
+        PS> Set-DwImportDeviceFeed -ImportId 1 -Name "My New Import Name" -Enabled $false -Instance "myinstance.dashworks.app" -APIKey "xxxxx"
+
+    #>
+    [CmdletBinding(SupportsShouldProcess)]
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Instance,
+        [Parameter(Mandatory=$false)]
+        [int]$Port = 8443,
+        [Parameter(Mandatory=$true)]
+        [string]$APIKey,
+        [parameter(Mandatory=$true)]
+        [int]$ImportId,
+        [parameter(Mandatory=$false)]
+        [string]$Name,
+        [parameter(ParameterSetName = 'FeedEnabled', Mandatory = $false)]
+        [bool]$Enabled
+    )
+
+    if (-Not $Name -And -Not $PSCmdlet.ParameterSetName -eq 'FeedEnabled') {
+        throw "Either Name or Enabled must be specified."
+    }
+
+    $uri = "https://{0}:{1}/apiv2/imports/devices/{2}" -f $Instance, $Port, $ImportId
+    $headers = @{'x-api-key' = $APIKey}
+
+    $payload = @{}
+    if ($name) { $payload.Add("name", $Name) }
+    if ($PSCmdlet.ParameterSetName -eq 'FeedEnabled') { $payload.Add("enabled", $Enabled) }
+
+    $jsonBody = $payload | ConvertTo-Json
+
+    try {
+        if ($PSCmdlet.ShouldProcess($ImportId)) {
+            $result = Invoke-RestMethod -Uri $uri -Method PATCH -Headers $headers -ContentType "application/json" -Body $jsonBody
+            return $result
+        }
+    }
+    catch {
+        if ($_.Exception.Response.StatusCode.Value__ -eq 409)
+        {
+            Write-Error ("{0}" -f "Update conflicted with another feed. Check if another feed exists with the same name.")
+            break
+        }
+        else {
+            Write-Error ("{0}. {1}" -f $_.Exception.Response.StatusCode.Value__, ($_ | ConvertFrom-Json).details)
+            break
+        }
+    }
+}
