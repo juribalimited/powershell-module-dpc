08904fe15db7a610b5ec38a4cb383ec8a7b1072b
Neil Wheeler <neil.wheeler@juriba.com>
2022-04-13 12:01:11 +0100

    updates from peer review

--- Examples/MECM/MECM Object List into Collection.ps1
+++ Examples/MECM/MECM Object List into Collection.ps1
@@ -15,43 +15,43 @@ for a user based list. These are required to update the task values for the obje
 The list used should contain a filter to exclude the success and failed values of the task
 that is to be updated. For example, the list might pick up the "Not Started" values in an
 NNSFC created task, once these have been changed to Complete or Failed, they will no longer
-appear in the task
+appear in the task.
 
 #>
 
 [CmdletBinding()]
 param (
-[Parameter(Mandatory=$true)]
-[string]$DwInstance,
-[Parameter(Mandatory=$true)]
-[string]$DwAPIKey,
-[Parameter(Mandatory=$true)]
-[int]$ListID,
-[Parameter(Mandatory=$true)]
-[int]$ProjectID,
-[Parameter(Mandatory=$true)]
-[int]$TaskID,
-[Parameter(Mandatory=$true)]
-[int]$successValue,
-[Parameter(Mandatory=$true)]
-[int]$failValue,
-[Parameter(Mandatory=$true)]
-[string]$TargetCollectionName,
-[Parameter(Mandatory = $true)]
-[ValidateSet("Device", "User")]
-[string]$ObjectType
+    [Parameter(Mandatory=$true)]
+    [string]$DwInstance,
+    [Parameter(Mandatory=$true)]
+    [string]$DwAPIKey,
+    [Parameter(Mandatory=$true)]
+    [int]$ListID,
+    [Parameter(Mandatory=$true)]
+    [int]$ProjectID,
+    [Parameter(Mandatory=$true)]
+    [int]$TaskID,
+    [Parameter(Mandatory=$true)]
+    [int]$SuccessValue,
+    [Parameter(Mandatory=$true)]
+    [int]$FailValue,
+    [Parameter(Mandatory=$true)]
+    [string]$TargetCollectionName,
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("Device", "User")]
+    [string]$ObjectType
 )
 
 #Configure the MECM Powershell module
 Set-Location "$env:SMS_ADMIN_UI_PATH\..\"
-import-module '.\ConfigurationManager.psd1'
+Import-Module '.\ConfigurationManager.psd1'
 
 #This works for SCCM -> may need adjustment of the SMS WMI provider is not present.
-$CMServer=(Get-CimInstance -class SMS_ProviderLocation -Namespace root\SMS).Machine
+$CMServer = (Get-CimInstance -class SMS_ProviderLocation -Namespace root\SMS).Machine
 
 #Set the drive path in order to allow use of the MECM Cmdlets
-$Drv = Get-PSDrive -Name "DW1" -ErrorAction SilentlyContinue
-if ($null -eq $Drv)
+$drv = Get-PSDrive -Name "DW1" -ErrorAction SilentlyContinue
+if ($null -eq $drv)
 {
     New-PSDrive -Name "DW1" -PSProvider "CMSite" -Root $CMServer -Description "Primary site"
 }
@@ -59,7 +59,7 @@ Set-Location DW1:\
 
 
 #Pull the list that requires processing
-$List = export-DwList -Instance $DwInstance -APIKey $DwAPIKey -ListId $listID -ObjectType $ObjectType
+$List = Export-DwList -Instance $DwInstance -APIKey $DwAPIKey -ListId $listID -ObjectType $ObjectType
 
 #Get the details of the collection the items are to be added to.
 $CMCollection = Get-CMCollection -Name $TargetCollectionName
@@ -84,12 +84,12 @@ forEach($row in $List)
         if ($ObjectType -eq 'Device')
         {
             Add-CMDeviceCollectionDirectMembershipRule -CollectionId $CMCollection.CollectionID -ResourceId $CMdevice.ResourceID
-            Set-DwTaskValueSelect -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'Device Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $successValue
+            Set-DwTaskValueSelect -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'Device Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $SuccessValue
         }
         else
         {
             Add-CMUserCollectionDirectMembershipRule -CollectionId $CMCollection.CollectionID -ResourceId $CMUser.ResourceID
-            Set-DwTaskValueSelect -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'User Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $successValue
+            Set-DwTaskValueSelect -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'User Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $SuccessValue
         }
     }
     catch
@@ -97,11 +97,11 @@ forEach($row in $List)
         #On failure, set the task value to the provided fail value and continue.
         if ($ObjectType -eq 'Device')
         {
-            Set-DwRadioButtonTaskValue -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'Device Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $failValue
+            Set-DwRadioButtonTaskValue -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'Device Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $FailValue
         }
         else
         {
-            Set-DwRadioButtonTaskValue -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'User Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $failValue
+            Set-DwRadioButtonTaskValue -Instance $DwInstance -APIKey $DwAPIKey -ObjectKey $($row.'User Key') -ObjectType $ObjectType -TaskId $TaskID -ProjectId $ProjectID -Value $FailValue
         }
     }
 }
