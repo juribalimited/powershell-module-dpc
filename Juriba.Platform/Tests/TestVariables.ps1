de558ff179ce07f1c9b59919d27da20f791c70ad
CheokHo Mai <cheokho@hotmail.co.uk>
2024-01-09 12:24:45 +0000

    Add better error handling and introduce test variables

---
+++ Juriba.Platform/Tests/TestVariables.ps1
@@ -0,0 +1,8 @@
+    <#
+        .DESCRIPTION
+        File used to store universal variables for all tests that will be dot sourced for all test files.
+    #>
+
+$apiKey = ""
+$importId = ""
+$instance = "https://.internal.juriba.com:8443"
