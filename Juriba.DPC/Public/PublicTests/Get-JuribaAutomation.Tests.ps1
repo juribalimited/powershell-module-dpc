53471e2ba701f38760e58b44d669d8f881431097
shak-juriba <shakib.ahmed@juriba.com>
2025-01-17 16:30:36 +0000

    a

--- Juriba.DPC/Public/PublicTests/Get-JuribaAutomation.Tests.ps1
+++ Juriba.DPC/Public/PublicTests/Get-JuribaAutomation.Tests.ps1
@@ -1,6 +1,15 @@
 # All Test files should import this
 #Import-Module
 Set-Location -Path $PSScriptRoot
+Write-Host "PSScriptRoot: $PSScriptRoot"
+Write-Host "Attempting to dot-source BaseTest.ps1"
+
+if (Test-Path "$PSScriptRoot\BaseTest.ps1") {
+    Write-Host "Dot-sourcing BaseTest.ps1"
+    . $PSScriptRoot\BaseTest.ps1
+} else {
+    Write-Host "BaseTest.ps1 not found at $PSScriptRoot\BaseTest.ps1"
+}
 
 . $PSScriptRoot\BaseTest.ps1
 
--- Juriba.DPC/Public/PublicTests/Set-JuribaImportUser.Tests.ps1
+++ Juriba.DPC/Public/PublicTests/Set-JuribaImportUser.Tests.ps1
@@ -2,21 +2,5 @@
 . ".\BaseTest.ps1"
 
 Describe "Set-JuribaImportUser" {
-    BeforeAll {
-        Setup-TestEnvironment
-        
-    }
-    Context "Acceptance test" {
-    It "Get an automation back and verify not null" {
-       try {
-
-        $output = Get-JuribaAutomation -Instance $instance -APIKey $apiKey
-        $output | Should Not Be $null
-        }
-        catch {
-            Write-Error $_
-            Break
-        }
-    }
-    }
+   
 }
