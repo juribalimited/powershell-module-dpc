f4520caa1e291e40af31707e5d031e14e701cbcd
meganshiner <69020150+meganshiner@users.noreply.github.com>
2026-07-17 13:34:25 +0100

    Meganshiner patch CSV user and device imports (#149)
    > We are allowing contributions for now with limited review whilst we
    learn what common practices, approach and recommendations we can follow
    prior to promoting these modules to our customers.
    
    ### Manual Tasks Checklist
    
    - [ ] Increment module version number in
    /Juriba.Platform/Juriba.Platform.psd1
    - [ ] Add new functions to 'FunctionsToExport' in
    /Juriba.Platform/Juriba.Platform.psd1

--- Examples/CSV/CSV Device Import.ps1
+++ Examples/CSV/CSV Device Import.ps1
@@ -2,11 +2,11 @@
 
 .SYNOPSIS
 A sample script to query a CSV file for devices and import
-those devices into Dashworks.
+those devices into Juriba DPC.
 
 .DESCRIPTION
 A sample script to query a CSV file for devices and import
-those devices into Dashworks. Script will either update or create
+those devices into Juriba DPC. Script will either update or create
 the device.
 
 #>
@@ -14,28 +14,28 @@ the device.
 [CmdletBinding()]
 param (
     [Parameter(Mandatory=$true)]
-    [string]$DwInstance,
+    [string]$JuribaInstance,
     [Parameter(Mandatory=$true)]
-    [string]$DwAPIKey,
+    [string]$JuribaAPIKey,
     [Parameter(Mandatory=$true)]
-    [string]$DwFeedName,
+    [string]$JuribaFeedName,
     [Parameter(Mandatory=$true)]
     [string]$Path
 )
 
 #Requires -Version 7
-#Requires -Module @{ ModuleName = 'Juriba.Dashworks'; ModuleVersion = '0.0.14' }
+#Requires -Module @{ ModuleName = 'Juriba.DPC'; ModuleVersion = '0.0.14' }
 
-$DashworksParams = @{
-    Instance = $DwInstance
-    APIKey = $DwAPIKey
+$JuribaParams = @{
+    Instance = $JuribaInstance
+    APIKey = $JuribaAPIKey
 }
 
-# Get DW feed
-$feed = Get-DwImportDeviceFeed @DashworksParams -Name $DwFeedName
+# Get Juriba DPC feed
+$feed = Get-JuribaImportDeviceFeed @JuribaParams -Name $JuribaFeedName
 # If it doesnt exist, create it
 if (-Not $feed) {
-    $feed = New-DwImportDeviceFeed @DashworksParams -Name $DwFeedName -Enabled $true
+    $feed = New-JuribaImportDeviceFeed @JuribaParams -Name $JuribaFeedName -Enabled $true
 }
 $importId = $feed.id
 
@@ -44,24 +44,46 @@ Write-Information ("Using feed id {0}" -f $importId) -InformationAction Continue
 # Get data from CSV file
 $csvFile = Import-Csv -Path $Path
 
+<# Optional Format File data
+$columnsToRemove = @("ImportID")
+#Validate that columns exist before removing
+    foreach ($col in $columnsToRemove) {
+        if ($csvFile[0].PSObject.Properties.Name -contains $col) {
+            $csvFile | ForEach-Object { $_.PSObject.Properties.Remove($col) }
+        }
+        else {
+            Write-Warning "Column '$col' not found in CSV. Skipping."
+        }
+    }
+
+#add owner field.
+$csvFile = $csvFile | Select-Object *,@{Name='owner'; Expression={"/imports/" + $ImportID + "/users/" + $_."User Principal Name"}}
+foreach ($line in $csvFile) {
+    $emptyline = "/imports/" + $ImportID + "/users/"
+    if ($line.owner -eq $emptyline) {
+        $line.owner = ''
+    }
+}
+#>
+
 $i = 0
 foreach ($line in $csvFile) {
     $i++
     # convert line to json
     $jsonBody = $line | ConvertTo-Json
     $uniqueIdentifier = $line.uniqueIdentifier
-    Write-Progress -Activity "Importing Devices to Dashworks" -Status ("Processing device: {0}" -f $uniqueIdentifier) -PercentComplete (($i/$csvFile.Count*100))
+    Write-Progress -Activity "Importing Devices to Juriba DPC" -Status ("Processing device: {0}" -f $uniqueIdentifier) -PercentComplete (($i/$csvFile.Count*100))
 
-    $existingDevice = Get-DwImportDevice @DashworksParams -ImportId $importId -UniqueIdentifier $uniqueIdentifier
+    $existingDevice = Get-JuribaImportDevice @JuribaParams -ImportId $importId -UniqueIdentifier $uniqueIdentifier
     if ($existingDevice) {
-        $result = Set-DwImportDevice @DashworksParams -ImportId $importId -UniqueIdentifier $uniqueIdentifier -JsonBody $jsonBody
+        $result = Set-JuribaImportDevice @JuribaParams -ImportId $importId -UniqueIdentifier $uniqueIdentifier -JsonBody $jsonBody
         # check result, for an update we are expecting status code 204
         if ($result.StatusCode -ne 204) {
             Write-Error $result
         }
     }
     else {
-        $result = New-DwImportDevice @DashworksParams -ImportId $importId -JsonBody $jsonBody
+        $result = New-JuribaImportDevice @JuribaParams -ImportId $importId -JsonBody $jsonBody
         #check result, for a new device we expect the return object to contain the device
         if (-Not $result.uniqueIdentifier) {
             Write-Error $result
--- Examples/CSV/CSV User Import.ps1
+++ Examples/CSV/CSV User Import.ps1
@@ -1,38 +1,38 @@
 <#
 .SYNOPSIS
 A sample script to query a CSV file for users and import
-those users into Dashworks.
+those users into Juriba DPC.
 .DESCRIPTION
 A sample script to query a CSV file for users and import
-those users into Dashworks. Script will either update or create
+those users into Juriba DPC. Script will either update or create
 the user.
 #>
 
 [CmdletBinding()]
 param (
     [Parameter(Mandatory=$true)]
-    [string]$DwInstance,
+    [string]$JuribaInstance,
     [Parameter(Mandatory=$true)]
-    [string]$DwAPIKey,
+    [string]$JuribaAPIKey,
     [Parameter(Mandatory=$true)]
-    [string]$DwFeedName,
+    [string]$JuribaFeedName,
     [Parameter(Mandatory=$true)]
     [string]$Path
 )
 
 #Requires -Version 7
-#Requires -Module @{ ModuleName = 'Juriba.Dashworks'; ModuleVersion = '0.0.14' }
+#Requires -Module @{ ModuleName = 'Juriba.DPC'; ModuleVersion = '0.0.14' }
 
-$DashworksParams = @{
-    Instance = $DwInstance
-    APIKey = $DwAPIKey
+$JuribaParams = @{
+    Instance = $JuribaInstance
+    APIKey = $JuribaAPIKey
 }
 
-# Get DW feed
-$feed = Get-DwImportUserFeed @DashworksParams -Name $DwFeedName
+# Get Juriba DPC feed
+$feed = Get-JuribaImportUserFeed @JuribaParams -Name $JuribaFeedName
 # If it doesnt exist, create it
 if (-Not $feed) {
-    $feed = New-DwImportUserFeed @DashworksParams -Name $DwFeedName -Enabled $true
+    $feed = New-JuribaImportUserFeed @JuribaParams -Name $JuribaFeedName -Enabled $true
 }
 $importId = $feed.id
 
@@ -41,24 +41,38 @@ Write-Information ("Using feed id {0}" -f $importId) -InformationAction Continue
 # Get data from CSV file
 $csvFile = Import-Csv -Path $Path
 
+<# Optional Format File data
+$columnsToRemove = @("ImportID")
+#Validate that columns exist before removing
+    foreach ($col in $columnsToRemove) {
+        if ($csvFile[0].PSObject.Properties.Name -contains $col) {
+            $csvFile | ForEach-Object { $_.PSObject.Properties.Remove($col) }
+        }
+        else {
+            Write-Warning "Column '$col' not found in CSV. Skipping."
+        }
+    }
+#>
+
 $i = 0
 foreach ($line in $csvFile) {
     $i++
     # convert line to json
     $jsonBody = $line | ConvertTo-Json
     $username = $line.username
-    Write-Progress -Activity "Importing Users to Dashworks" -Status ("Processing User: {0}" -f $username) -PercentComplete (($i/$csvFile.Count*100))
+    $UniqueIdentifier = $line.UniqueIdentifier
+    Write-Progress -Activity "Importing Users to Juriba DPC" -Status ("Processing User: {0}" -f $username) -PercentComplete (($i/$csvFile.Count*100))
 
-    $existingUser = Get-DwImportUser @DashworksParams -ImportId $importId -Username $username
+    $existingUser = Get-JuribaImportUser @JuribaParams -ImportId $importId -Username $username
     if ($existingUser) {
-        $result = Set-DwImportUser @DashworksParams -ImportId $importId -UniqueIdentifier $username -JsonBody $jsonBody
+        $result = Set-JuribaImportUser @JuribaParams -ImportId $importId -UniqueIdentifier $UniqueIdentifier -JsonBody $jsonBody
         # check result, for an update we are expecting status code 204
         if ($result.StatusCode -ne 204) {
             Write-Error $result
         }
     }
     else {
-        $result = New-DwImportUser @DashworksParams -ImportId $importId -JsonBody $jsonBody
+        $result = New-JuribaImportUser @JuribaParams -ImportId $importId -JsonBody $jsonBody
         #check result, for a new user we expect the return object to contain the user
         if (-Not $result.username) {
             Write-Error $result
