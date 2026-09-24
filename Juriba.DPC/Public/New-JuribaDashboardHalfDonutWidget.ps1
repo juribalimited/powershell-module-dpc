8899eac5ae48c1d7c3a02690efca859f7a3225a2
meganshiner <69020150+meganshiner@users.noreply.github.com>
2026-09-24 11:29:07 +0100

    Fix legend value for 'Bottom' position

--- Juriba.DPC/Public/New-JuribaDashboardHalfDonutWidget.ps1
+++ Juriba.DPC/Public/New-JuribaDashboardHalfDonutWidget.ps1
@@ -97,7 +97,7 @@ function New-JuribaDashboardHalfDonutWidget {
         "Right" {$LegendValue = 2}
         "Left" {$LegendValue = 3}
         "Top" {$LegendValue = 4}
-        "Bottom" {$LegendValue = 4}
+        "Bottom" {$LegendValue = 5}
     }
     switch ($OrderByField){
         "Split Value" {$OrderById = 1}
